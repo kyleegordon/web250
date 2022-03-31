173c7df6a9a89d1c0f44716f4f52b4ceb329e866
Kylee Gordon <kgordo19@email.cpcc.edu>
2022-03-31 10:03:17 -0400

    add dynamically loaded php version of each page

---
+++ dynamic/contents/toc.php
@@ -0,0 +1,28 @@
+<div class="container-fluid text-responsive">
+    <div id="bg">
+        <!-- image credit: Peter Gargiulo, unsplash.com  -->
+        <img src="https://images.unsplash.com/photo-1578662996442-48f60103fc96?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            alt="" />
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <div data-include="components/header.html"></div>
+            <main class="main">
+                <div class="row text-center">
+                    <div class="col-sm-12 h2">
+                        <h2 class="assignment-title">Table of Contents</h2>
+                    </div>
+                </div>
+                <div class="row dflex justify-content-center">
+                    <div class="col-auto h4">
+                        <ul id="toc_list">
+                            <li><a href="?p=contract.php">Contract</a></li>
+                            <li><a href="?p=introduction.php">Introduction</a></li>
+                            <li><a href="?p=brand.php">Brand</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </div>
+</div>
